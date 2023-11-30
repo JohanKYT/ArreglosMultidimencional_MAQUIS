f541d7e3fd330b4c72ba1f478f66103ca009ee0c
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-11-30 16:47:15 -0500

    Definimos el numero de amtrices y las dimensiones y ponemos la funcion para que lea lo que ingresa el usuario

--- Arreglomultidimencional.c
+++ Arreglomultidimencional.c
@@ -2,4 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+int main()
+{
+    // Definimos las variables del numero de matrices y las dimeciones de las mismas
+    int numMatrices, n, m;
+    printf("Ingrese el numero de matrices: ");
+    scanf("%d", &numMatrices);
+    // Escaneamos el numero de matrices
+    printf("Ingrese el numero de filas para la matriz: ");
+    scanf("%d", &n);
+    // Escaneamos el numero de filas de las matrices
+    printf("Ingrese el numero de columnas para la matriz: ");
+    scanf("%d", &m);
+    // Escaneamos el numero de filas de las matrices
+  
+    return 0;
+}
